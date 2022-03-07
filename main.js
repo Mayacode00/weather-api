ae9d573bd04d74f00de520f8af09e1f5772637ea
Mariam Ayoade <92252531+Mayacode00@users.noreply.github.com>
2022-03-07 17:20:35 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const timeEl = document.querySelector(".time");
 const dateEl = document.querySelector(".date");
 const place = document.querySelector(".place");
 const searchButton = document.querySelector(".search");
+const share = document.querySelector(".share-icon");
+const search = document.querySelector('.search')
 const currentWeatherItems = document.querySelector("#current-weather-items");
 const weatherForecast = document.querySelector("#weather-forecast");
 const currentDay = document.querySelector(".current-day");
@@ -29,6 +31,10 @@ const months = [
   "November",
   "December",
 ];
+share.addEventListener("click", () => {
+  let shareUrl = "https://twitter.com/intent/tweet?url";
+  window.open(shareUrl, "_blank");
+});
 const API_KEY = "ded7b3eafab45703fdd17ecd37f253e1";
 setInterval(() => {
   const time = new Date();
@@ -55,32 +61,31 @@ function getWeatherData() {
     let { latitude, longitude } = success.coords;
 
     fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&
-      lon=${longitude}&exclude=minutely,hourly&appid=${API_KEY}`
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=minutely,hourly&units=metric&appid=${API_KEY}`
     )
       .then((res) => res.json())
       .then((data) => {
-        showWeatherData(data);
         console.log(data);
+        showWeatherData(data);
       });
   });
 }
 function showWeatherData(data) {
   let { humidity, pressure, wind_speed, sunrise, sunset } = data.current;
 
-  place.innerHTML = data.place;
+  place.innerHTML = data.timezone;
 
   currentWeatherItems.innerHTML = `<div class="weather-item">
   <p>humidity</p>
-  <p>${humidity}</p>
+  <p>${humidity}%</p> 
 </div>
 <div class="weather-item">
   <p>Pressure</p>
-  <p>${pressure}</p>
+  <p>${pressure}hPa</p>
 </div>
 <div class="weather-item">
   <p>Windspeed</p>
-  <p>${wind_speed}</p>
+  <p>${wind_speed}m/s</p>
 </div>
 <div class="weather-item">
   <p>Sunrise</p>
@@ -102,17 +107,18 @@ function showWeatherData(data) {
         <p class="days" id="sunday">${window
           .moment(day.dt * 1000)
           .format("ddd")}</p>
-        <p class="day-temp">Day - ${day.temp.day}</p>
-        <p class="night-temp">Night - ${day.temp.night}</p>
+        <p class="day-temp">Day - ${day.temp.day}&#176c</p>
+        <p class="night-temp">Night - ${day.temp.night}&#176c</p>
       </div>`;
     } else {
       otherDayForecast += `
      <div class="weather-forecast-item">
         <p class="days">${window.moment(day.dt * 1000).format("ddd")}</p>
-        <img src="http://openweathermap.org/img/wn/${day.weather[0].icon}
-        @2x.png" alt="weather icon" class="w-icon">
-        <p class="day-temp">Day - ${day.temp.day}</p>
-        <p class="night-temp">Night - ${day.temp.night}</p>
+        <img src="http://openweathermap.org/img/wn/${
+          day.weather[0].icon
+        }@2x.png" alt="weather icon" class="w-icon">
+        <p class="day-temp">Day - ${day.temp.day}&#176c</p>
+        <p class="night-temp">Night - ${day.temp.night}&#176c</p>
       </div>`;
     }
   });
